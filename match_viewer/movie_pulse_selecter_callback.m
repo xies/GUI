2e281f1fac12c0fcc30ea8b4e7d68a12a8fe68d1
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2013-05-23 17:38:53 +0000

    Some scripts added

--- match_viewer/movie_pulse_selecter_callback.m
+++ match_viewer/movie_pulse_selecter_callback.m
@@ -27,8 +27,10 @@ if strcmpi( which,'trackID' )
 else
     % get all frames and bounds of non-nan frames
     this_pulse = fits.get_fitID( cats.(current_cat)(catID).(which) (whichID) );
-    min_nan = find(~isnan(handles.pulse.cells(this_pulse.stackID).dev_time),1,'first');
-    max_nan = find(~isnan(handles.pulse.cells(this_pulse.stackID).dev_time),1,'last');
+    min_nan = find(~isnan( ...
+        handles.pulse.cells.get_stackID(this_pulse.stackID).dev_time),1,'first');
+    max_nan = find(~isnan( ...
+        handles.pulse.cells.get_stackID(this_pulse.stackID).dev_time),1,'last');
     frames = [this_pulse.width_frames];
     frames = frames( frames >= min_nan & frames <= max_nan );
 end
